195ddc0a23ca3bab9a9687242dff9b7c2de252ec
kunalGonge <kunalgonge349@gmail.com>
2025-10-17 22:34:26 +0530

    arithmetic added

--- simple print.py
+++ simple print.py
@@ -1,4 +1,3 @@
 print('By this we will get more information in python')
 print('Just a try to know more about coding in python')
 print('Need more information to learn more about coding in python')
-print('arithmetic')
