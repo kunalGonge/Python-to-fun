3db7b6e5b6850012d60c588dbded51c0bdcbfc83
kunalGonge <kunalgonge349@gmail.com>
2025-10-17 22:30:50 +0530

    Your comment about the new code

---
+++ arithmetic operatres.py
@@ -0,0 +1,18 @@
+#In this we will see more about arithmetic operator
+num1 = 8
+num2 = 6
+
+#for add +
+print(num1 + num2)
+#for sub -
+print(num1 - num2)
+#for multiply *
+print(num1 * num2)
+#for divide /
+print(num1 / num2)
+#for divide // but this give only unit(non-decimal) value
+print(num1 // num2)
+#for reminder %
+print(num1 % num2)
+#for powar of any num
+print(num1 ** num2)
---
+++ simple print.py
@@ -0,0 +1,4 @@
+print('By this we will get more information in python')
+print('Just a try to know more about coding in python')
+print('Need more information to learn more about coding in python')
+print('arithmetic')
