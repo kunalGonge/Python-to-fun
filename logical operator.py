96f5b1da6aa9aaf127eda7c3afea9506a97806ba
kunalGonge <kunalgonge349@gmail.com>
2025-10-18 16:35:38 +0530

    logical operator.py is added

---
+++ logical operator.py
@@ -0,0 +1,7 @@
+#Here are some logical operator
+print(7==8)#to check equal to
+print(7>=6)#to check greater than or equal to
+print(7<=9)#to check less than or equal to
+print(7!=9)#to check not equal to
+print(7>7)#to check greater than
+print(9<2)#to check less than
