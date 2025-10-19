832172c1bf8c96bb9afa84f86a108725b4107424
kunalGonge <kunalgonge349@gmail.com>
2025-10-19 18:31:49 +0530

    intput method is added in logical operator.py

--- logical operator.py
+++ logical operator.py
@@ -1,7 +1,11 @@
 #Here are some logical operator
-print(7==8)#to check equal to
-print(7>=6)#to check greater than or equal to
-print(7<=9)#to check less than or equal to
-print(7!=9)#to check not equal to
-print(7>7)#to check greater than
-print(9<2)#to check less than
+x = int(input ('Enter x: '))
+y = int(input ('Enter y: '))
+print('x is:',x)
+print('y is:',y)
+print('both are same' if x==y else 'not same' )#to check equal to
+print('x is greater or equal' if x>=y else 'x is not greater or equal')#to check greater than or equal to
+print('y is greater or equal' if x<=y else 'y is not greater or equal')#to check less than or equal to
+print('both are not same' if x!=y else 'both are not same')#to check not equal to
+print('x is greater' if x>y else 'x is not greater')#to check greater than
+print('y is greater' if x<y else ' is greater')#to check less than
